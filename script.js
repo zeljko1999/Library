2e29ab50fa210e2c2f3469e0e7125fe86742d52f
zeljko1999 <zeljkoculibrk7@gmail.com>
2023-05-15 15:44:59 +0200

    Change book object proto to class

--- script.js
+++ script.js
@@ -21,12 +21,15 @@ form.addEventListener('submit', function (event) {
   form.reset()
 })
 
-function Book (title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.info = function () {
+class Book {
+  constructor (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+
+  info () {
     return (`${this.title} by ${this.author},${this.pages
     } pages, ${this.read}.`)
   }
